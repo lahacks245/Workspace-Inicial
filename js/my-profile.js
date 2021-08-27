fd64f77383b5f1ef1705b2279fd0005730fad7f5
lahacks245 <lahacks13@gmail.com>
2021-08-27 14:40:14 -0300

    nombre de conectado agregado

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,6 +1,15 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+function usuario(){
+    let user = JSON.parse(localStorage.getItem('usuario'))
+    document.getElementById('nombreusr').innerHTML +=  user.nombre 
+}
+
+
+
+
 document.addEventListener("DOMContentLoaded", function (e) {
+    usuario()
 
 });
