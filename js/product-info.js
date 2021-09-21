2f75a24a28a70d3f731d65cbaea169e64366a103
lahacks245 <lahacks13@gmail.com>
2021-09-21 20:44:17 -0300

    more chiches

--- js/product-info.js
+++ js/product-info.js
@@ -132,33 +132,37 @@ const showReviews = (productInfo) => {
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
-  getJSONData(PRODUCT_INFO_URL).then((resObj) => {
-    if (resObj.status === "ok") {
-      let product = resObj.data;
-      //console.log(product);
 
-      let productNameHTML = document.getElementById("productName");
-      let productDescriptionHTML =
-        document.getElementById("productDescription");
-      let productCostHTML = document.getElementById("productCost");
-      let productCurrencyHTML = document.getElementById("productCurrency");
-      let productSoldCountHTML = document.getElementById("productSoldCount");
-      let productCategoryHTML = document.getElementById("productCategory");
+  
 
-      productNameHTML.innerHTML = product.name;
-      productDescriptionHTML.innerHTML = product.description;
-      productCostHTML.innerHTML = product.cost;
-      productCurrencyHTML.innerHTML = product.currency;
-      productSoldCountHTML.innerHTML = product.soldCount;
-      productCategoryHTML.innerHTML = product.category;
-
-      showImagesGallery(product.images);
-    }
+ 
+  document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCT_INFO_URL).then((resObj) => {
+      if (resObj.status === "ok") {
+        let product = resObj.data;
+        //console.log(product);
+  
+        let productNameHTML = document.getElementById("productName");
+        let productDescriptionHTML =
+          document.getElementById("productDescription");
+        let productCostHTML = document.getElementById("productCost");
+        let productCurrencyHTML = document.getElementById("productCurrency");
+        let productSoldCountHTML = document.getElementById("productSoldCount");
+        let productCategoryHTML = document.getElementById("productCategory");
+  
+        productNameHTML.innerHTML = product.name;
+        productDescriptionHTML.innerHTML = product.description;
+        productCostHTML.innerHTML = product.cost;
+        productCurrencyHTML.innerHTML = product.currency;
+        productSoldCountHTML.innerHTML = product.soldCount;
+        productCategoryHTML.innerHTML = product.category;
+  
+        showImagesGallery(product.images);
+      }
+    });
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then((resultObj) => {
+      let productComments = resultObj.data;
+      console.log(productComments);
+      showReviews(productComments);
+    });
   });
-  getJSONData(PRODUCT_INFO_COMMENTS_URL).then((resultObj) => {
-    let productComments = resultObj.data;
-    console.log(productComments);
-    showReviews(productComments);
-  });
-});
